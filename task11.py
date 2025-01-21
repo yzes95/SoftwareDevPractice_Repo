e172755989344f218370815d15d67df85e736577
YahyaZE <yehia_mnofeh_1995@hotmail.com>
2025-01-21 19:49:03 +0200

    The files here got transfered to Week 1 Folder

--- task11.py
+++
@@ -1,7 +0,0 @@
-Cel = float(input("Enter the temp. in Fahrenheit: "))
-#assuming the user will co-operate and enter a float
-
-Fahr = ((9*Cel)/5)+32
-Cel = ((Fahr-32)*5)/9
-
-print(f"The temp. of {Cel} Celceius in Fahrenheit is : {Fahr} ")
--- task9.py
+++
@@ -1,6 +0,0 @@
-import re
-x,y,z= re.split(",| |/_",input("Enter three numbers to be added: "),flags = re.IGNORECASE)
-print("The sum is :",(int(x)+int(y)+int(z)))
-#Much more can be done using regular expressions 
-#Req to be revised later
-# The + sign can be used to add 2 of same type but not diff u cant add a string + int
