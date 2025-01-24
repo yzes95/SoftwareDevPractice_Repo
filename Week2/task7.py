a5c44e5787295895a9032ba1e85a63e40dc23ac7
YahyaZE <yehia_mnofeh_1995@hotmail.com>
2025-01-24 04:52:02 +0200

    Revised Task 7

---
+++ Week2/task7.py
@@ -0,0 +1,18 @@
+# task 7.a
+print("\nTask 7.a")
+my_number = 1.3
+print(my_number < 1)
+print(my_number > 1)
+print(my_number == 1)
+
+#task 7.b 
+print("\nTask 7.b")
+second_number = my_number/0.5+10
+some_value = 5
+print(second_number > 2.5*some_value)
+print(second_number < 2.5*some_value)
+print(second_number != 2.5*some_value)
+print(second_number = 2.5*some_value)
+
+#this task was about the results provided using comparison operator but there was an error in line 12 here 
+    #as it assiging values inside print fnc
