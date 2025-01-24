bcfbc75afd3ab33cd64beb77354a5b527aab2671
YahyaZE <yehia_mnofeh_1995@hotmail.com>
2025-01-24 04:58:51 +0200

    Revised Task 9

---
+++ Week2/task9.py
@@ -0,0 +1,10 @@
+my_bool1 = True
+my_bool2 = False
+
+print(f"Addit. of Boolean Var {my_bool1} and {my_bool2}: {my_bool1+my_bool2}")
+print(f"Subtr. of Boolean Var {my_bool1} and {my_bool2}: {my_bool1-my_bool2}")
+print(f"Multi. of Boolean Var {my_bool1} and {my_bool2}: {my_bool1*my_bool2}")
+print(f"Divis. of Boolean Var {my_bool1} and {my_bool2}: {my_bool1/my_bool2}")
+
+#all arthmetic operations will work fine as True and  False will be integrated as 1 and 0 respectively
+#last line didnt work cause it will divide by False which will be integrated as 0
