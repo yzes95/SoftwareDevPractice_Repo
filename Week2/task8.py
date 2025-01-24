8a3ff55b1772c57804d856c02b5fcee0998bb6cf
YahyaZE <yehia_mnofeh_1995@hotmail.com>
2025-01-24 04:54:15 +0200

    Revised Task 8

---
+++ Week2/task8.py
@@ -0,0 +1,8 @@
+my_bool1 = True
+my_bool2 = False
+print(my_bool1 or my_bool2)
+print(my_bool1 and my_bool2)
+new_bool = my_bool1 or my_bool2
+print(not(new_bool))
+
+# this task is for checking logical operators and how they function/perform
