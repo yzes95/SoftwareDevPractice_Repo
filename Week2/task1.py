9a563a71e6f5f47c0d5958b88d943944060d085a
YahyaZE <yehia_mnofeh_1995@hotmail.com>
2025-01-24 03:48:01 +0200

    Revised Task1

---
+++ Week2/task1.py
@@ -0,0 +1,20 @@
+a = 1
+b = 3.333333333
+c = -9884
+d = 3.552e04
+c = a + b
+d = [1, 3, 5, 7]
+e = True
+f = False
+g = (1, 3, 5, 7)
+
+print(
+    f"""The Data type for a is :{type(a)}
+          The Data type for b is :{type(b)}
+          The Data type for c is :{type(c)}
+          The Data type for d is :{type(d)}
+          The Data type for e is :{type(e)}
+          The Data type for f is :{type(f)}
+          The Data type for g is :{type(g)}
+       """
+)
