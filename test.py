79818360c8454b7704e22e2114a7c55374b04a97
YahyaZE <yehia_mnofeh_1995@hotmail.com>
2025-02-05 16:56:47 +0200

    Updating Repo Status

---
+++ test.py
@@ -0,0 +1,13 @@
+m =8
+l = 7
+for c in range(1,l,2):
+    r = m*(c-1)
+    print(r)
+
+
+id = input(" ur id :")
+print("thank you")
+print("id is :",id)
+
+x = "Welcome to University of Dundee!"
+print(len("Welcome to University of Dundee!"))
